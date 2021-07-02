fc0298aae70ca27d26d390798dab9c56bb423bfa
Rossano Marano <rox.marano@gmail.com>
2021-07-02 10:39:40 +0200

    First commit

---
+++ Assets/Udon_sharp_sketch/rispostaesatta.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class rispostaesatta : UdonSharpBehaviour
+{
+    public GameObject reveal;
+        public void RispostaEsatta()
+        {
+        Transform parent = transform.parent;
+        parent.gameObject.SetActive(false);        
+        reveal.SetActive(true);
+        Debug.Log("Risposta esatta");
+        }
+    
+}
